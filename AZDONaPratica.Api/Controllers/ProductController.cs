e9d4bd54eb1e9e85b486f338a0e8855221cbc82b
Milton Câmara <miltomcamara@gmail.com>
2019-08-31 09:48:50 -0300

    First Commit

---
+++ AZDONaPratica.Api/Controllers/ProductController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AZDONaPratica.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AZDONaPratica.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+            return new List<Product>
+            {
+                new Product("Huawei MediaPad", "new_1.jpg", 500, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum laoreet."),
+                new Product("Huawei MediaPad", "new_5.jpg", 500, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum laoreet."),
+                new Product("Huawei MediaPad", "new_8.jpg", 500, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum laoreet."),
+                new Product("Huawei MediaPad", "best_3.png", 500, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum laoreet."),
+                new Product("Huawei MediaPad", "featured_2.png", 500, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum laoreet."),
+                new Product("Huawei MediaPad", "featured_8.png", 500, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum laoreet."),
+                new Product("Huawei MediaPad", "new_1.jpg", 500, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum laoreet."),
+                new Product("Huawei MediaPad", "new_1.jpg", 500, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum laoreet."),
+                new Product("Huawei MediaPad", "new_1.jpg", 500, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum laoreet."),
+                new Product("Huawei MediaPad", "new_1.jpg", 500, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum laoreet."),
+                new Product("Huawei MediaPad", "new_1.jpg", 500, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum laoreet."),
+                new Product("Huawei MediaPad", "new_1.jpg", 500, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum laoreet.")
+            };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ AZDONaPratica.Api/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AZDONaPratica.Api.Models
+{
+    public class Product
+    {
+        public Product(string name, string image, decimal amount, string description)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Amount = amount;
+            Description = description;
+            Image = image;
+        }
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+    }
+}
